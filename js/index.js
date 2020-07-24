c62f253c190c18cd3abc2e13d3705d52c226a62c
brahianf <brahianf@unicauca.edu.co>
2020-07-23 20:02:28 -0500

    Show modal con datos

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 
-const API = 'https://api.bestbuy.com/v1/products((categoryPath.id=abcat0502000))?apiKey=//&pageSize=12&format=json';
+const API = 'https://api.bestbuy.com/v1/products((categoryPath.id=abcat0502000))?apiKey=nnsnWRzwWhuucvJc59z56TfR&pageSize=12&format=json';
 
 (async function load(){
 
@@ -28,15 +28,15 @@ const API = 'https://api.bestbuy.com/v1/products((categoryPath.id=abcat0502000))
     return data
   }
 
+  const productList= await cacheExist('products');
+  console.log(productList)
+
   function productItemtemplate(product) {
-    // let name = ;
-    // name=name;
     return (
       `
       <div class="carousel__item fadeIn" id="primaryProductlist" data-id=${product.sku}>
         <img class="carousel-item__img" src="${product.image}" alt=""/>
         <div class="carousel-item__details">
-          <div><img src="https://img.icons8.com/flat_round/64/000000/plus.png" alt=""/></div>
           <p class="carousel-item__details--title">${(product.name).substring(0,20)}</p>
           <p class="carousel-item__details--subtitle">$ ${product.regularPrice}</p>
         </div>
@@ -45,62 +45,75 @@ const API = 'https://api.bestbuy.com/v1/products((categoryPath.id=abcat0502000))
     )
   }
 
+  const $modal = document.getElementById('modal')
+  const $modalTitle = $modal.querySelector('h1')
+  const $modalImage = $modal.querySelector('img')
+  const $modalDescription = $modal.querySelector('p')
+  // const $modalPrice = $modal.querySelector('h1')
+  const $overlay = document.getElementById('overlay')
+  const $hideModal = document.getElementById('hide-modal')
+
+  function findbyId(id){
+    // debugger
+    return productList.find(product => product.sku === parseInt(id,10));
+  }
+
+
+  function showModal($element) {
+    $overlay.classList.add('active')
+    $modal.style.animation = 'modalIn .8s forwards'
+    const id = $element.dataset.id
+    const data = findbyId(id)
+    $modalTitle.textContent = data.name
+    $modalImage.setAttribute('src', data.image);
+    $modalDescription.textContent = data.longDescription
+    // $modalPrice.textContent=data.regularPrice
+  }
+
+
+  function addEventClick($element) {
+    $element.addEventListener('click', () => {
+      // alert('click')
+      showModal($element)
+    })
+  }
+
   function  renderProductList(list, $container){
     $container.children[0].remove()
     list.forEach((product) => {
       const HTMLString = productItemtemplate(product);
-      // console.log(HTMLString)
+      // console.log(HTMLString
       $container.innerHTML += HTMLString
-      // const $primaryProductlist = document.getElementById('primaryProductlist')
-      // const image = $container.querySelector('img')
+      const $primaryProductlist = document.querySelectorAll('#primaryProductlist');
+      $primaryProductlist.forEach(item => addEventClick(item));
+
+      // const image = $container.querySelectorAll('img')
       // image.addEventListener('load', (event) => {
-        // $image.classList.add('fadeIn')
-        // event.srcElement.classList.add('fadeIn')
+      //   // $image.classList.add('fadeIn')
+      //   event.srcElement.classList.add('fadeIn')
       // })
-      // addEventClick($primaryProductlist);
     });
   }
 
-  const productList= await cacheExist('products');
-  console.log(productList)
+  // console.log(productList)
   const $carouselContainer = document.querySelector('#carousel')
   renderProductList(productList, $carouselContainer)
 
-  
-//      // Utilizar variable $modal ya rastreada para buscar #modal img
-//      const $modal = document.getElementById('modal')
-//      const $modalTitle = $modal.querySelector('h1')
-//      const $modalImage = $modal.querySelector('img')
-//      const $modalDescription = $modal.querySelector('p')
-//      $primaryPlaylist = document.getElementById('item"')
-  
-//      function showModal($element) {
-//       $overlay.classList.add('active')
-//       $modal.style.animation = 'modalIn .8s forwards'
-//     //   const id = $element.dataset.id
-//     //   const category = $element.dataset.category
-//     //   const data = findMovie(id,category)
-//     //   $modalTitle.textContent = data.title
-//     //   $modalImage.setAttribute('src', data.medium_cover_image);
-//     //   $modalDescription.textContent = data.description_fulls
-//     }
-//     // showModal()
-//     function addEventClick($element) {
-//       $element.addEventListener('click', () => {
-//         // alert('click')
-//         showModal($element)
-//       })
-//     }
-//     // console.log(videoItemtemplate('src/images/covers/bitcoin.jpg','Bitcoin'))
-//     addEventClick($primaryPlaylist);
+  const $add = document.querySelectorAll('#add');
+  const $sub = document.querySelectorAll('#sub');
+  $add.forEach(item => item.addEventListener('click', addToCart(item)));
+  // $sub.forEach(item => item.addEventListener('click', subToCart(item)));
 
-//     const $overlay = document.getElementById('overlay')
-//     const $hideModal = document.getElementById('hide-modal')
-  
-//     $hideModal.addEventListener('click', hideModal)
-//     function hideModal(){
-//       $overlay.classList.remove('active')
-//       $modal.style.animation = 'modalOut .8s forwards'
-//     }
-  
-  })()
+  function addToCart(item){
+    const id = item.dataset.id
+    const product= findbyId(id);
+    console.log(product);
+  }
+
+  $hideModal.addEventListener('click', hideModal)
+  function hideModal(){
+    $overlay.classList.remove('active')
+    $modal.style.animation = 'modalOut .8s forwards'
+  }
+
+})()
